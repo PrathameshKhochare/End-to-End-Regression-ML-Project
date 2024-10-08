e0fd9fb4806e88f3145c40abfdd1a005b1e7d053
PrathameshKhochare <khochareprathamesh10@gmail.com>
2024-10-08 16:03:24 +0530

    Data Ingection added

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # End-to-End-Regression-ML-Project
 
 # Workflows
-Update config.yaml
-Update schema.yaml
-Update params.yaml
-Update the entity
-Update the configuration manager in src config
-Update the components
-Update the pipeline
-Update the main.py
-Update the app.py
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
+9. Update the app.py
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from MLRegression import logger
+from MLRegression.pipeline.stage_01_data_ingection import DataIngestionTrainingPipeline
 
-logger.info("This is my Regression project")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+    
