4985863054dfff9ab98fc909f472cbec5eba1e15
PrathameshKhochare <khochareprathamesh10@gmail.com>
2024-10-13 23:29:56 +0530

    app deployed using Flask

--- src/MLRegression/pipeline/stage_05_model_evaluation_rf.py
+++ src/MLRegression/pipeline/stage_05_model_evaluation_rf.py
@@ -11,7 +11,7 @@ class ModelEvaluationTrainingPipeline:
 
     def main(self):
         config = ConfigurationManager()
-        model_evaluation_config = config.()
+        model_evaluation_config = config.get_model_evaluation_config()
         model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
         model_evaluation_config.evaluate()
 
